f3a737df02c87de8b1745927de9ffbcf06baaf9a
刀疤亮亮 <332591960@qq.com>
2022-03-20 21:31:57 +0800

    完善读取总箱单数据和CD出证部分

--- work_by_python/truck_with_mark_new/test.py
+++ work_by_python/truck_with_mark_new/test.py
@@ -1,3 +1,4 @@
+from cmath import nan
 from operator import index
 from openpyxl import load_workbook, Workbook
 import numpy as np
@@ -28,27 +29,52 @@ def copy_imformation_by_head_cell():
     wb.save('总箱单表-12点00修改.xlsx')
     wb.close
 
+# 读取总的箱单数据，并且按照提单号和卡车号排序并出CD申请
+def read_infor_and_sort_xlsx():
+    infor_by_pd = pd.read_excel('总箱单表-12点00修改.xlsx','总箱单')
 
-infor_by_pd = pd.read_excel('总箱单表-12点00修改.xlsx','总箱单')
-# print(infor_by_pd.head(10))
+    # 按照车头号的值进行排序
+    # reset.index() 对排序的df对象重新建立索引
+
+    sorted_df = infor_by_pd.sort_values(by='提单号')
+    sorted_df = infor_by_pd.sort_values(by='车头号').reset_index()
+    # print(sorted_df.head(50))
+    # print(sorted_df.tail(50))
+    # print(len(sorted_df))
+    # print(type(sorted_df.loc[2000,'车头号']))
+
+    # 用来储存一个CD单证所需卡车信息的index
+    TheIndexOf_OnTrucksGoodsInformation = []
+    
+    # 单证所需卡车index的列表
+    list_of_all_CD_that_needed_index =[]
+
+    # 遍历整个表格中的数据，输出{车头号+提单尾号：[信息]} 
+    for n in range(0,2000):
+        
+        # 如果车头号为空，证明货物还没有装车，所以退出循环
+        if pd.isna(sorted_df.loc[n,'车头号']) == True:
+            break
+        
+        # 如果上一行车头号、提单号和下一行均相同，则证明这两列的货物装在同一辆车上
+        elif sorted_df.loc[n,'车头号'] == sorted_df.loc[n+1, '车头号']:
+            if sorted_df.loc[n,'提单号'] == sorted_df.loc[n+1,'提单号']:
+                # 车牌号，提单号都相同，继续读取
+                TheIndexOf_OnTrucksGoodsInformation.append(n)
+            else:
+                # 提单号不同，一张CD完成
+                TheIndexOf_OnTrucksGoodsInformation.append(n)
+                # 出单，调用出单函数
+                TheIndexOf_OnTrucksGoodsInformation = []
+                list_of_all_CD_that_needed_index.append(TheIndexOf_OnTrucksGoodsInformation)
+        else:
+            # 车牌号不同，一张CD完成
+            TheIndexOf_OnTrucksGoodsInformation.append(n)
+            # 出单，调用出单函数
+            TheIndexOf_OnTrucksGoodsInformation = []
+            list_of_all_CD_that_needed_index.append(TheIndexOf_OnTrucksGoodsInformation)
+    
+    print(list_of_all_CD_that_needed_index)        
 
 
-# for x in infor_by_pd:
-#     print(x)
-
-
-# print(len(infor_by_pd))
-
-# for x in range(0,len(infor_by_pd)):
-#     print(infor_by_pd.loc[x,'车头号'])
-
-
-
-# 按照车头号的值进行排序
-# reset.index() 对排序的df对象重新建立索引
-sorted_df = infor_by_pd.sort_values(by='车头号').reset_index()
-print(sorted_df)
-# print(type(infor_by_pd))
-# 
-for n in range(0,30):
-    print(sorted_df.loc[n,'车头号'])
+read_infor_and_sort_xlsx()
