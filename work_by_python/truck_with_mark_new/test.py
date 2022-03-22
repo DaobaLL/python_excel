ca60baddfa76c2250d7e387e5314f4845b18df22
刀疤亮亮 <332591960@qq.com>
2022-03-22 15:04:34 +0800

    新增按照对排序后的列表输出为xlsx

--- work_by_python/truck_with_mark_new/test.py
+++ work_by_python/truck_with_mark_new/test.py
@@ -4,7 +4,12 @@ from openpyxl import load_workbook, Workbook
 import numpy as np
 import pandas as pd
 from openpyxl.utils.dataframe import dataframe_to_rows
+import time
 
+
+global index_xlsx
+index_xlsx = 1
+# 一次性函数，复制取消的合并单元格
 def copy_imformation_by_head_cell():
     wb = load_workbook('总箱单表-12点00修改.xlsx')
     ws = wb['总箱单']
@@ -29,15 +34,21 @@ def copy_imformation_by_head_cell():
     wb.save('总箱单表-12点00修改.xlsx')
     wb.close
 
-# 读取总的箱单数据，并且按照提单号和卡车号排序并出CD申请
-def read_infor_and_sort_xlsx():
+# 读取总的箱单数据，返回经过排序的df
+def read_infor():
+    
     infor_by_pd = pd.read_excel('总箱单表-12点00修改.xlsx','总箱单')
+    # infor_by_pd = pd.read_csv('总箱单表-12点00修改.csv',encoding='gbk')
 
     # 按照车头号的值进行排序
-    # reset.index() 对排序的df对象重新建立索引
+    sorted_df = infor_by_pd.sort_values(by='提单号',ascending=False)
+    sorted_df = sorted_df.sort_values(by='车头号',ascending=True).reset_index()
+    return sorted_df
+    
 
-    sorted_df = infor_by_pd.sort_values(by='提单号')
-    sorted_df = infor_by_pd.sort_values(by='车头号').reset_index()
+# 按照提单号和卡车号排序并出CD申请所需要列的列表
+def to_sort_xlsx():
+    sorted_df = read_infor()
     # print(sorted_df.head(50))
     # print(sorted_df.tail(50))
     # print(len(sorted_df))
@@ -49,8 +60,11 @@ def read_infor_and_sort_xlsx():
     # 单证所需卡车index的列表
     list_of_all_CD_that_needed_index =[]
 
+    wb = Workbook()
+    ws = wb.active
+    write_title(ws)
     # 遍历整个表格中的数据，输出{车头号+提单尾号：[信息]} 
-    for n in range(0,2000):
+    for n in range(0,len(sorted_df)):
         
         # 如果车头号为空，证明货物还没有装车，所以退出循环
         if pd.isna(sorted_df.loc[n,'车头号']) == True:
@@ -65,16 +79,77 @@ def read_infor_and_sort_xlsx():
                 # 提单号不同，一张CD完成
                 TheIndexOf_OnTrucksGoodsInformation.append(n)
                 # 出单，调用出单函数
-                TheIndexOf_OnTrucksGoodsInformation = []
                 list_of_all_CD_that_needed_index.append(TheIndexOf_OnTrucksGoodsInformation)
+                putout_cd_application(TheIndexOf_OnTrucksGoodsInformation, ws)
+                TheIndexOf_OnTrucksGoodsInformation = []
         else:
             # 车牌号不同，一张CD完成
             TheIndexOf_OnTrucksGoodsInformation.append(n)
             # 出单，调用出单函数
-            TheIndexOf_OnTrucksGoodsInformation = []
             list_of_all_CD_that_needed_index.append(TheIndexOf_OnTrucksGoodsInformation)
+            putout_cd_application(TheIndexOf_OnTrucksGoodsInformation, ws)
+            TheIndexOf_OnTrucksGoodsInformation = []
     
-    print(list_of_all_CD_that_needed_index)        
+    wb.save('created_cd_application.xlsx')
+    wb.close()
+    print('You\'ve done your works, go home NOW!!!!!')
+    return list_of_all_CD_that_needed_index
 
 
-read_infor_and_sort_xlsx()
+# 出单函数
+# 写入表头，执行一次即可
+def write_title(ws):
+    # 写入表头
+    ws['A' + str(1)]='提单号'
+    ws['B' + str(1)]='批次号  Column2'
+    ws['C' + str(1)]='车头号'
+    ws['D' + str(1)]='产品中文名称  Goods Name CN'
+    ws['E' + str(1)]='产品英文名称   Goods Name EN'
+    ws['F' + str(1)]='毛重'
+    ws['G' + str(1)]='商品编码   Hs code.'
+    ws['H' + str(1)]='数量'
+    ws['I' + str(1)]='CIF 总价   Total CIF PRICE (USD)'
+    ws['J' + str(1)]='FOB 总价  Total FOB PRICE  (USD)'
+    ws['K' + str(1)]='保险   ISSURANCE (USD)'
+    ws['L' + str(1)]='商业发票号   commercial inv no.'
+    ws['L' + str(1)]='Purchase order  PO'
+    return None
+
+    
+def putout_cd_application(TheIndexOf_OnTrucksGoodsInformation, ws):
+    #  声明全局变量
+    global index_xlsx
+
+
+    sorted_df = read_infor()
+    # 遍历传入的一张cd所需的列表,
+    for n in TheIndexOf_OnTrucksGoodsInformation:
+        # print(sorted_df.loc[n,]) 
+        ws['A' + str(index_xlsx+1)]=sorted_df.loc[n,'提单号']
+        ws['B' + str(index_xlsx+1)]=sorted_df.loc[n,'批次号  Column2']
+        ws['C' + str(index_xlsx+1)]=sorted_df.loc[n,'车头号']
+        ws['D' + str(index_xlsx+1)]=sorted_df.loc[n,'产品中文名称  Goods Name CN']
+        ws['E' + str(index_xlsx+1)]=sorted_df.loc[n,'产品英文名称   Goods Name EN']
+        ws['F' + str(index_xlsx+1)]=sorted_df.loc[n,'毛重']
+        ws['G' + str(index_xlsx+1)]=sorted_df.loc[n,'商品编码   Hs code.']
+        ws['H' + str(index_xlsx+1)]=sorted_df.loc[n,'数量']
+        ws['I' + str(index_xlsx+1)]=sorted_df.loc[n,'CIF 总价   Total CIF PRICE (USD)']
+        ws['J' + str(index_xlsx+1)]=sorted_df.loc[n,'FOB 总价  Total FOB PRICE  (USD)']
+        ws['K' + str(index_xlsx+1)]=sorted_df.loc[n,'保险   ISSURANCE (USD)']
+        ws['L' + str(index_xlsx+1)]=sorted_df.loc[n,'商业发票号   commercial inv no.']
+        ws['M' + str(index_xlsx+1)]=sorted_df.loc[n,'Purchase order  PO']
+
+        index_xlsx += 1
+
+    
+    return None
+
+
+#计算时间消耗
+start = time.time()
+
+to_sort_xlsx()
+
+end = time.time()
+print("循环运行时间:%.2f秒"%(end-start))
+#output:循环运行时间秒
